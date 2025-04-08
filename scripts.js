729ccbf6314981cb6302e1b46d8608ae92d4cf3b
Arsentii.P <arsentii.poltavchenko@powercoders.org>
2025-04-08 15:26:12 +0200

    JS game initialization

---
+++ scripts.js
@@ -0,0 +1,27 @@
+// words array
+
+// randomizer to pick the word
+
+// user's HP (tries left)
+
+// guessed letters array
+
+// guessed letters to render
+
+// func: is there letter in the word?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
