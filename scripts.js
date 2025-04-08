a4fda7b67b5dbb19e503e035a4b3b20795fbd423
valentina <valentina.morelos@powercoders.org>
2025-04-08 16:17:18 +0200

    Added game beginning with user promt and instruction window

--- scripts.js
+++ scripts.js
@@ -11,6 +11,13 @@
 // func: is there letter in the word?
 
 
+/* 【The Hangman】Hello User ! Please help me, the evil a.i. wants to hang me for treason, only you can stop them by guessing the secret word one letter at a time! Please be careful, you only have 6 attempts before they teleport my whole body into the hanging chamber. */
+
+let userName = prompt("Please write your user name, if you dare! ໒(⊙ᴗ⊙)७✎▤");
+if (userName){
+    alert("Welcome "+ userName + " to 【 ⬱ The Hangman ⇶ 】 Please help me, the evil a.i. wants to execute me for treason ☠, only you can stop them by guessing the secret word one letter at a time! Please be careful, you only have 6 attempts before they teleport my whole body into the hanging chamber (´⊙ω⊙`)！");
+} else { alert("You did not enter a username >:(");
+}
 
 
 
